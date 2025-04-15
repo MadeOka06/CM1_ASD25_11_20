5666a1dfa4b9ad5ea27a06857a8f4e023fa96eb6
rahmanlana <rahmanlana044@gmail.com>
2025-04-15 09:49:05 +0700

    commit_barang

--- src/CASE_METHOD/Barang11_20.java
+++ src/CASE_METHOD/Barang11_20.java
@@ -1,5 +1,30 @@
 package CASE_METHOD;
 
 public class Barang11_20 {
+    String kodeBarang;
+    String nama;
+    String kategori;
+    int stock;
+    int harga;
+
+    Barang11_20() {
+
+    }
+
+    Barang11_20(String kodeBarang, String nama, String kategori, int stock, int harga) {
+        kodeBarang = kodeBarang;
+        nama = nama;
+        kategori = kategori;
+        stock = stock;
+        harga = harga;
+    }
+
+    void tampilDataBarang() {
+        System.out.println("==========================================");
+        System.out.println("| Kode Barang | Nama Barang | Stock | Harga |");
+        System.out.println("==========================================");
+        System.out.printf("| %-11s | %-11s | %-5d | %-5d |\n", kodeBarang, nama, stock, harga);
+        System.out.println("==========================================");
+    }
 
 }
