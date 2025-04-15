af65eea68c6c59090cf27c321573989161bf8c68
MadeOka06 <okadarmestha@gmail.com>
2025-04-15 10:00:29 +0700

    commit 2

---
+++ src/CASE_METHOD/TransaksiPembelian11_20.java
@@ -0,0 +1,13 @@
+package CASE_METHOD;
+public class TransaksiPembelian11_20 {
+    public static void main(String[] args) {
+        Barang11_20 [] barang = { 
+            new Barang11_20("Br001", "Leo Kripik", "Makanan", 10, 1000),
+            new Barang11_20("Br002", "You C1000 Botol", "Minuman", 10, 7000),
+            new Barang11_20("Br003", "Teh Pucuk Botol", "Makanan", 10, 4000),
+            new Barang11_20("Br004", "Rinso Cair", "Makanan", 10, 7000),
+            new Barang11_20("Br005", "Mama Lemon Cair", "Makanan", 10, 1000),
+        }; 
+    }
+    
+}
