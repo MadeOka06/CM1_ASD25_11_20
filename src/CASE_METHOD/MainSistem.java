4e36706210fa1b91c4c569b480eca006aff30472
MadeOka06 <okadarmestha@gmail.com>
2025-04-15 12:47:06 +0700

    Commti Terakhir

--- src/CASE_METHOD/MainSistem.java
+++ src/CASE_METHOD/MainSistem.java
@@ -8,30 +8,29 @@ public class MainSistem {
     public static void main(String[] args) {
         Barang11_20 list = new Barang11_20();
         TransaksiPembelian11_20 tr = new TransaksiPembelian11_20();
-        SercviceClass11_20 src = new SercviceClass11_20();
         Scanner sc = new Scanner(System.in);
         Barang11_20[] barang = {
-                new Barang11_20("Br001", "Leo Kripik", "Makanan", 10, 1000),
-                new Barang11_20("Br002", "You C1000 Botol", "Minuman", 10, 7000),
-                new Barang11_20("Br003", "Teh Pucuk Botol", "Makanan", 10, 4000),
-                new Barang11_20("Br004", "Rinso Cair", "Makanan", 10, 7000),
-                new Barang11_20("Br005", "Mama Lemon Cair", "Makanan", 10, 1000),
+            new Barang11_20("Br001", "Leo Kripik", "Makanan", 10, 1000),
+            new Barang11_20("Br002", "You C1000 Botol", "Minuman", 10, 7000),
+            new Barang11_20("Br003", "Teh Pucuk Botol", "Makanan", 10, 4000),
+            new Barang11_20("Br004", "Rinso Cair", "Makanan", 10, 7000),
+            new Barang11_20("Br005", "Mama Lemon Cair", "Makanan", 10, 1000),
         };
         TransaksiPembelian11_20[] Transaksi = {
-                new TransaksiPembelian11_20("Tr001", "Santi", "21-04-2024", "Leo Kripik", 1, 1000),
-                new TransaksiPembelian11_20("Tr002", "Vani", "21-04-2024", "You C1000 Botol", 2, 7000),
-                new TransaksiPembelian11_20("Tr003", "Siska", "21-04-2024", "Teh Pucuk Botol", 3, 4000),
-                new TransaksiPembelian11_20("Tr004", "MeiMei", "21-04-2024", "Teh Pucuk Botol", 1, 4000),
-                new TransaksiPembelian11_20("Tr005", "Ihsan", "21-04-2024", "You C1000 Botol", 1, 7000),
+            new TransaksiPembelian11_20("Tr001", "Santi", "21-04-2024", "Leo Kripik", 1, 1000),
+            new TransaksiPembelian11_20("Tr002", "Vani", "21-04-2024", "You C1000 Botol", 2, 7000),
+            new TransaksiPembelian11_20("Tr003", "Siska", "21-04-2024", "Teh Pucuk Botol", 3, 4000),
+            new TransaksiPembelian11_20("Tr004", "MeiMei", "21-04-2024", "Teh Pucuk Botol", 1, 4000),
+            new TransaksiPembelian11_20("Tr005", "Ihsan", "21-04-2024", "You C1000 Botol", 1, 7000),
         };
+        SercviceClass11_20 src = new SercviceClass11_20(Transaksi, 0);
         while (true) {
             System.out.println("============== TOKO Manasuka =============");
             System.out.println("1. Data Barang");
             System.out.println("2. Data Transaksi");
             System.out.println("3. Pencarian Data");
             System.out.println("4. Pengurutan Data");
-            System.out.println("5. Tampilkan Kode Terbalik");
-            System.out.println("6. Exit");
+            System.out.println("5. Exit");
             System.out.print("Chose Menu ( 1-6 ) : ");
             int menu = sc.nextInt();
 
@@ -52,13 +51,10 @@ public class MainSistem {
             } else if (menu == 3) {
                 src.tambahDataTransaksi(Transaksi);
             } else if (menu == 4) {
-
+                src.tampilUrut();
             } else if (menu == 5) {
-
-            } else if (menu == 6) {
                 break;
             }
         }
-
     }
 }
--- src/CASE_METHOD/SercviceClass11_20.java
+++ src/CASE_METHOD/SercviceClass11_20.java
@@ -6,9 +6,14 @@ public class SercviceClass11_20 {
     TransaksiPembelian11_20[] trs;
     int index;
 
+    SercviceClass11_20(TransaksiPembelian11_20[] trs, int index){
+        this.trs = trs;
+        this.index = index;
+    }
+
     void tambahDataTransaksi(TransaksiPembelian11_20[] Transaksi) {
         Scanner sc = new Scanner(System.in);
-        trs = new TransaksiPembelian11_20[4];
+        this.trs = Transaksi;
 
         System.out.println("Masukkan kode transaksi yang ingin anda cari");
         System.out.print("Kode: ");
@@ -31,7 +36,7 @@ public class SercviceClass11_20 {
         }
         return posisi;
     }
-
+    
     void tampilPosisi(String n, int posi) {
         if (posi != -1) {
             System.out.println("Data Kode Transaksi dengan " + n + " ditemukan pada index ke-" + posi);
@@ -39,23 +44,43 @@ public class SercviceClass11_20 {
             System.out.println("Data " + n + " tidak temukan");
         }
     }
-
     void tampilDataSearch(int posi) {
         if (posi != -1) {
-            for (TransaksiPembelian11_20 m : trs) {
-                m.tampilDataTransaksi();
-                System.out.println("------------------------------------------------");
-            }
+            TransaksiPembelian11_20 data = trs[posi];
+            System.out.printf(" %-14s  %-16s  %-20s %-15s %-9d  %-5d \n",
+                    data.kodeTransaksi, data.namaPembelian, data.tanggalPembelian,
+                    data.brg, data.qty, data.hrg);
         } else {
             System.out.println("Data Tidak ditemukan");
         }
     }
-    // UpdateData(){
+    void bubbleSort() {
+        if (trs == null) {
+            System.out.println("Data transaksi belum diatur. Harap set terlebih dahulu.");
+            return;
+        }
+        for (int i = 0; i < trs.length - 1; i++) {
+            for (int j = 0; j < trs.length - 1 - i; j++) {
+                if (trs[j] != null && trs[j + 1] != null &&
+                    trs[j].kodeTransaksi.compareToIgnoreCase(trs[j + 1].kodeTransaksi) > 0) {
+    
+                    TransaksiPembelian11_20 temp = trs[j];
+                    trs[j] = trs[j + 1];
+                    trs[j + 1] = temp;
+                }
+            }
+        }
+    }
+        void tampilUrut() {
+           bubbleSort();
+            System.out.println("Data transaksi setelah diurutkan berdasarkan kode transaksi:");
+            for (TransaksiPembelian11_20 data : trs) {
+                if (data != null) {
+                    System.out.printf(" %-14s  %-16s  %-20s %-15s %-9d  %-5d \n",
+                            data.kodeTransaksi, data.namaPembelian, data.tanggalPembelian,
+                            data.brg, data.qty, data.hrg);
+                }
+            }
+        }
+    }
 
-    // }
-
-    // Sorting(){
-
-    // }
-
-}
